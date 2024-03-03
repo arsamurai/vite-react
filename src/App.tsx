a9e09abd6d4266fad24900151e3c6bd22d2e2d50
Arsamurai <musienkoartur45@gmail.com>
2024-03-03 21:55:19 +0200

    c-6

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ const App: React.FC = () => {
 					<h1 className="font-bold underline text-red-500 flex justify-center">
             Hello world!</h1>
 
-          <p className="font-bold text-red-400 underline h-32">Text
+          <p className="font-bold text-red-400 underline h-32">
+            Text
           </p>
 				</div>
 			</div>
