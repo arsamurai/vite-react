3fd183a161dea542c2957a338b8df06fa24e252b
Arsamurai <musienkoartur45@gmail.com>
2024-03-03 22:04:03 +0200

    c-12

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
 const App: React.FC = () => {
-	return (
-		<div className="app">
-			<div className="container">
-				<div className="app__inner">
-					<h1 className="font-bold  text-red-500 flex justify-center underline">
-            Hello world!</h1>
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="app__inner">
+          <h1
+            className="flex  justify-center font-bold text-red-500 underline
+          "
+          >
+            Hello world!
+          </h1>
 
-          <p className="font-bold text-red-400 underline h-32">
-            Text
-          </p>
-				</div>
-			</div>
-		</div>
-	);
+          <p className="h-32 font-bold text-red-400 underline">Text</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
