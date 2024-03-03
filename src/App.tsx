dc08fc373f878920b901a341e27dba9b35101546
Arsamurai <musienkoartur45@gmail.com>
2024-03-03 21:47:52 +0200

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ const App: React.FC = () => {
 			<div className="container">
 				<div className="app__inner">
 					<h1 className="font-bold underline text-red-500 flex justify-center">Hello world!</h1>
-          <p className="font-bold text-red-400 underline h-32 w-  ">Text</p>
+
+          <p className="font-bold text-red-400 underline h-32">Text
+          </p>
 				</div>
 			</div>
 		</div>
