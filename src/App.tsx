268c176418000ced126558f261dd645a97826fa4
Arsamurai <musienkoartur45@gmail.com>
2024-03-03 21:45:41 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<div className="app">
+			<div className="container">
+				<div className="app__inner">
+					<h1 className="font-bold underline text-red-500 flex justify-center">Hello world!</h1>
+          <p className="font-bold text-red-400 underline h-32 w-  ">Text</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
