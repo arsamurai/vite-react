fbf9be317c521cf1859c29ca5925b56442903e32
Arsamurai <musienkoartur45@gmail.com>
2024-03-03 22:05:35 +0200

    c-13

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const App: React.FC = () => {
+  const func = () => {
+    const b = '1';
+    const a = '2';
+
+    return a + b;
+  };
+
   return (
     <div className="app">
       <div className="container">
         <div className="app__inner">
-          <h1
-            className="flex  justify-center font-bold text-red-500 underline
-          "
-          >
-            Hello world!
-          </h1>
-
-          <p className="h-32 font-bold text-red-400 underline">Text</p>
+          <h1 className="flex  justify-center font-bold text-red-500 underline ">Hello world!</h1>
+          <p className="h-32 font-bold text-red-400 underline">{func()}</p>
         </div>
       </div>
     </div>
